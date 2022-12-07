fc7ac122bc4227e2151a46b154da6a7dafcca1e0
gilebr1 <gil.lebrun@orange.com>
2022-12-07 10:55:37 +0100

    initial commit

---
+++ app/src/main/java/fr/enssat/osm/MapActivity.kt
@@ -0,0 +1,29 @@
+package fr.enssat.osm
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import org.osmdroid.config.Configuration
+
+class MapActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Configuration pour Open Street Map
+        Configuration.getInstance().load(this, getSharedPreferences("osm", Context.MODE_PRIVATE))
+
+        setContent {
+            TregorRandoTheme {
+                 Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) { MapDetails()}
+            }
+        }
+    }
+}
